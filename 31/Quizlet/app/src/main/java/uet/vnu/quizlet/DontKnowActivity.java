be13953b92e9242bdcda97e7fc99b9a758b42611
= <=>
2020-10-29 09:28:54 +0700

    update

--- 31/Quizlet/app/src/main/java/uet/vnu/quizlet/DontKnowActivity.java
+++ 31/Quizlet/app/src/main/java/uet/vnu/quizlet/DontKnowActivity.java
@@ -6,29 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class WriteActivity extends AppCompatActivity {
-
+public class DontKnowActivity extends AppCompatActivity {
     EditText editTextDataEntry;
     TextView textViewWordInput;
-    ProgressBar progressBarProgressRegister;
-    Button buttonHint;
-
+    ProgressBar  progressBarProgressRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_write);
+        setContentView(R.layout.activity_dont_know);
         editTextDataEntry = (EditText) findViewById(R.id.editTextDataEntry);
         textViewWordInput = (TextView) findViewById(R.id.textViewWordInput);
-        progressBarProgressRegister = (ProgressBar) findViewById(R.id.progressBarProgressRegister);
-        buttonHint = (Button) findViewById(R.id.buttonHint);
-        Intent intent = getIntent();
-        int data = intent.getIntExtra("progressBarCurrent",0);
-        progressBarProgressRegister.setProgress(data);
+        progressBarProgressRegister = (ProgressBar) findViewById(R.id. progressBarProgressRegister);
+        Intent pbCurrent = getIntent();
+        int current = pbCurrent.getIntExtra("progressBarCurrent",0);
+        //cong them 20 khi dung
+        progressBarProgressRegister.setProgress( current);
         editTextDataEntry.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -45,32 +41,18 @@ public class WriteActivity extends AppCompatActivity {
                 return false;
             }
         });
-        buttonHint.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(WriteActivity.this, DontKnowActivity.class);
-                int current  = progressBarProgressRegister.getProgress();
-                intent.putExtra("progressBarCurrent",current);
-                startActivity(intent);
-            }
-        });
-
     }
 
     public void correctAnswer() {
         String str = null;
         editTextDataEntry.setText(str);// set lai editText
-        Intent intent =new Intent(WriteActivity.this,WriteCorrectActivity.class);
-        int current  = progressBarProgressRegister.getProgress();
-        intent.putExtra("progressBarCurrent",current);
+        Intent intent =new Intent(DontKnowActivity.this,WriteCorrectActivity.class);
         startActivity(intent);
     }
     public void incorrectAnswer() {
-
-        Intent intent =new Intent(WriteActivity.this,WriteIncorrectActivity.class);
+        Intent intent =new Intent(DontKnowActivity.this,WriteIncorrectActivity.class);
         String dapAn = editTextDataEntry.getText().toString();
         intent.putExtra("dataEntry",dapAn);
-        int current  = progressBarProgressRegister.getProgress();
-        intent.putExtra("progressBarCurrent",current);
         startActivity(intent);
     }
 }
